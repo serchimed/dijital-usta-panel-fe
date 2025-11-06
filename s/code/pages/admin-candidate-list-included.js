0b74554af61dc9bea218a65c9b1b60ce7881e5da
serchimed <229318842+serchimed@users.noreply.github.com>
2025-11-06 14:55:32 +0300

    css changes

--- s/code/pages/admin-candidate-list-included.js
+++ s/code/pages/admin-candidate-list-included.js
@@ -7,8 +7,10 @@ onAuthReady(async () => {
     for (let candidate of result.data) {
       let $tr = tr();
       $tr.append(tda(candidate.displayName, "admin-candidate-profile.html?id=" + candidate.id));
+      $tr.append(td(candidate.email));
       $tr.append(td(candidate.city));
       $tr.append(td(candidate.status));
+      $tr.append(td(formatDateTimeLong(candidate.wordpressRegisterDate)));
       tbody.append($tr);
     }
   } else { logErr(result); }
